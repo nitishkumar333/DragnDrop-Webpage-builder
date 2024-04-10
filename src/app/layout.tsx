60f03679ea1db9b5cf751122318e0b21f6cd96b6
nitishkumar333 <ae.nitishkumar@gmail.com>
2024-04-10 20:28:27 +0530

    change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { Toaster as SonnarToaster } from "@/components/ui/sonner";
 const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Plura",
-  description: "All in one Agency Solution",
+  title: "Builder",
+  description: "No code webpage builder!",
 };
 
 export default function RootLayout({
